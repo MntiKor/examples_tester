0fc6b46cdb0407303dbce3e966b15b2ed1bc7e47
Adrien Stucky <adrien.stucky@kitware.com>
2023-10-09 16:52:12 +0200

    first try to build everything for object storage

--- package_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-emsdk_path=$1
-
-for f in Data/*
-do
-    filename=$(basename ${f})
-    ${emsdk_path}/upstream/emscripten/tools/file_packager packaged_data/${filename}.data --preload ${f}@/ --js-output=packaged_data/${filename}.js
-done
