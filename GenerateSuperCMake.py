3fdbb03b88edafd1e7c2a7239bb5a10cc2320d57
Adrien Stucky <adrien.stucky@kitware.com>
2023-11-14 14:04:14 +0100

    test parallel build using cmake and ninja

---
+++ GenerateSuperCMake.py
@@ -0,0 +1,14 @@
+import shutil
+import glob
+
+shutil.copyfile('CMakeLists_global.txt.template', 'pregen/CMakeLists.txt')
+with open('pregen/CMakeLists.txt', 'r') as cmake:
+    data = cmake.read()
+
+subdirectories = []
+for path in glob.glob('pregen/**/CMakeLists.txt', recursive=True):
+    subdirectories.append('add_subdirectory(' + path.removeprefix('pregen/').removesuffix('CMakeLists.txt') + ')')
+data.replace('XXX', '\n'.join(subdirectories))
+
+with open('pregen/CMakeLists.txt', 'w') as cmake:
+    cmake.write(data)
--- build_all.sh
+++ build_all.sh
@@ -44,46 +44,38 @@ for topic in sources/*
 do
 	topic_name=$(basename ${topic})
 	./GenerateExamplesWASM.sh ${vtk_dir} ${topic} pregen/${topic_name}
-	for example in pregen/${topic_name}/*
+done
+
+python3 GenerateSuperCMake.py
+emcmake cmake -GNinja -DEMSCRIPTEN:Bool=true -DVTK_DIR=${vtk_dir}/build -DDEBUGINFO=NONE -S pregen -B build
+cmake --build build
+
+for topic in build/*
+do
+	for example in ${topic}/*
 	do
 		example_name=$(basename ${example})
-		build_dir=build/${topic_name}/${example_name}
-		mkdir -p ${build_dir}
-
-		emcmake cmake -GNinja -DEMSCRIPTEN:Bool=true -DVTK_DIR=${vtk_dir}/build -DDEBUGINFO=NONE -S ${example} -B ${build_dir}
-
-		if [ $? -ne 0 ]
-		then
-			echo ${example_name} >> ${repo}/doesntcompile.txt
-		else
-			cmake --build ${build_dir}
-			if [ $? -ne 0 ]
-			then
-				echo ${example_name} >> ${repo}/doesntcompile.txt
-			else
-				gzip ${build_dir}/${example_name}.wasm
-				aws s3api put-object \
-					--bucket vtk-wasm-examples \
-					--key ${example_name}/${example_name}.wasm \
-					--body ${build_dir}/${example_name}.wasm.gz \
-					--content-encoding gzip \
-					--acl public-read \
-					--content-type application/wasm
-				gzip ${build_dir}/${example_name}.js
-				aws s3api put-object \
-					--bucket vtk-wasm-examples \
-					--key ${example_name}/${example_name}.js \
-					--body ${build_dir}/${example_name}.js.gz \
-					--content-encoding gzip \
-					--acl public-read
-				gzip ${build_dir}/index.html
-				aws s3api put-object \
-					--bucket vtk-wasm-examples \
-					--key ${example_name}/index.html \
-					--body ${build_dir}/index.html.gz \
-					--content-encoding gzip \
-					--acl public-read
-			fi
-		fi
+		gzip ${example}/${example_name}.wasm
+		aws s3api put-object \
+			--bucket vtk-wasm-examples \
+			--key ${example_name}/${example_name}.wasm \
+			--body ${example}/${example_name}.wasm.gz \
+			--content-encoding gzip \
+			--acl public-read \
+			--content-type application/wasm
+		gzip ${example}/${example_name}.js
+		aws s3api put-object \
+			--bucket vtk-wasm-examples \
+			--key ${example_name}/${example_name}.js \
+			--body ${example}/${example_name}.js.gz \
+			--content-encoding gzip \
+			--acl public-read
+		gzip ${example}/index.html
+		aws s3api put-object \
+			--bucket vtk-wasm-examples \
+			--key ${example_name}/index.html \
+			--body ${example}/index.html.gz \
+			--content-encoding gzip \
+			--acl public-read
 	done
 done
